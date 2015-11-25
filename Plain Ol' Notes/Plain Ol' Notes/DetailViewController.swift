74ed68d7c400fd327e1562c40b703f6ca678ed6f
Paul <comrade_pasha@ukr.net>
2015-11-25 19:27:33 +0200

    v001

--- Plain Ol' Notes/Plain Ol' Notes/DetailViewController.swift
+++ Plain Ol' Notes/Plain Ol' Notes/DetailViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+//    @IBOutlet weak var detailDescriptionLabel: UILabel!
+    @IBOutlet weak var detailDescriptionLabel: UITextView!
 
 
     var detailItem: AnyObject? {
